a8661e0202c5a302e28427bfd392adf16ec9adc7
Alvaro Rocha <alvarowebrj@gmail.com>
2026-05-25 20:58:16 -0300

    feat(modulo09): Adicionado: pericias, ouro, inventario

--- modulo09/exercicioRPG01/application/Program.java
+++ modulo09/exercicioRPG01/application/Program.java
@@ -3,45 +3,25 @@ package modulo09.exercicioRPG01.application;
 import java.util.Locale;
 import java.util.Scanner;
 import modulo09.exercicioRPG01.entities.Character;
-
 public class Program {
     
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-
-        
+        Scanner sc = new Scanner(System.in);        
 
         System.out.print("Insert the Character name: ");
-        String characterName = sc.nextLine();
-        
+        String characterName = sc.nextLine();        
 
         System.out.print("Insert the Character sex (f/m): ");
         char characterSex = sc.next().charAt(0);
 
-        String characterClass = "";
-        System.out.print("Choose the character class: (1) Warrior | (2) Mage | (3) Cleric | (4) - Thief: - ");
-        
+       //Character Class
+        System.out.print("Choose the character class: (1) Warrior | (2) Mage | (3) Cleric | (4) - Thief: - ");        
         int classResponse = sc.nextInt();
 
-        switch (classResponse) {
-            case 1:
-                characterClass = "Warrior";
-                break;
-            case 2:
-                characterClass = "Mage";
-                break;
-            case 3:
-                characterClass = "Cleric";
-                break;
-            case 4:
-                characterClass = "Thief";
-                break;        
-            default:
-                characterClass = "Peasant";
-                break;
-        }
+        sc.nextLine();        
 
+        //Character Attributes
         System.out.print("Character's Strength: ");
         int str = sc.nextInt();
         System.out.print("Character's Dexterity: ");
@@ -51,21 +31,34 @@ public class Program {
         System.out.print("Character's Charisma: ");
         int cha = sc.nextInt();
 
-
-         System.out.print("Choose the character class: (1) Human | (2) Elf | (3) Dwarf | (4) - Hobbit | ");
+        //Character Race
+         System.out.print("Choose the character Race: (1) Human | (2) Elf | (3) Dwarf | (4) - Hobbit | ");
          int raceResponse = sc.nextInt();
          
+         sc.nextLine();
 
-        Character character = new Character(characterName, characterSex, characterClass, str, dex, wis, cha, raceResponse);
+         //Character Skills
+         System.out.println("Você tem " + wis + " perícia(s).");
+         String characterSkills = "";
 
-        character.chooseRace(raceResponse);
+        for (int i = 0; i < wis; i++) {
+            System.out.print( (i+1) + "ª Perícia: ");
+            String skill = sc.nextLine();
 
+            if(i == 0) {
+                characterSkills += skill;
+            } else {
+                characterSkills += ", " + skill;
+            }
+        }               
 
+        //Character constructor
+        Character character = new Character(characterName, characterSex, classResponse, str, dex, wis, cha, raceResponse, characterSkills);
+
+        //Character Sheet
         System.out.println();
         System.out.println("Character Sheet: ");
         System.out.println(character);
-
-
 
         sc.close();
     }
--- modulo09/exercicioRPG01/entities/Character.java
+++ modulo09/exercicioRPG01/entities/Character.java
@@ -2,6 +2,7 @@ package modulo09.exercicioRPG01.entities;
 
 public class Character {    
 
+    //Private Fields
     private String characterName;
     private char characterSex;
     private String characterClass;  
@@ -9,23 +10,28 @@ public class Character {
     private int dex;
     private int wis;
     private int cha;
-    private int raceResponse;
+    private String characterSkills;
+    private String characterRace;
+    private double movement;
+    private String inventory;
+    private double gold;
+    
 
-    public Character (String characterName, char characterSex, String characterClass, int str, int dex, int wis, int cha, int raceResponse) {
+    //Character Constructor
+    public Character (String characterName, char characterSex, int classResponse, int str, int dex, int wis, int cha, int raceResponse, String characterSkills) {
         this.characterName = characterName;
-        this.characterSex = characterSex;
-        this.characterClass = characterClass;
+        this.characterSex = characterSex;       
         this.str = str;
         this.dex = dex;
         this.wis = wis;
         this.cha = cha;
-        this.raceResponse = raceResponse;
-    }
-
-    String characterRace = "";   
-    double movement = 0;
-    public void chooseRace(int raceResponse) {      
-
+        this.characterSkills = characterSkills;
+        chooseRace(raceResponse);
+        chooseClass(classResponse);
+    }      
+    
+    //Choose Race and Bônus
+    private void chooseRace(int raceResponse) {  
         switch (raceResponse) {
             case 1:
                 characterRace = "Human";
@@ -41,30 +47,64 @@ public class Character {
                 characterRace = "Dwarf";
                 str += 1;
                 cha -= 1;
-                movement =  dex * 1;
+                movement =  dex * 1.0;
                 break;
             case 4:
                 characterRace = "Hobbit";
                 str -= 1;
                 cha += 1;
-                movement =  dex * 1;
+                movement =  dex * 1.0;
                 break;        
             default:
                 characterRace = "Goblin";
+                 movement =  dex * 0.75;
                 break;
         }
-
     }
 
+    //Choose Class, Inventory and Gold
+    private void chooseClass (int classResponse) {
+        switch (classResponse) {
+            case 1:
+                characterClass = "Warrior";
+                inventory = "Sword, Shield, Heavy Armor";
+                gold = cha * 1.5;                
+                break;
+            case 2:
+                characterClass = "Mage";
+                inventory = "Magical Staff, Grimoire, Robe";
+                gold = cha * 2.5;
+                break;
+            case 3:
+                characterClass = "Cleric";
+                inventory = "Mace, Shield, Sacred Medallion";
+                gold = cha * 1;
+                break;
+            case 4:
+                characterClass = "Thief";
+                inventory = "Cloak, Dagger, Poison";
+                gold = cha * 2.0;
+                break;        
+            default:
+                characterClass = "Peasant";
+                inventory = "Bread, Water, Axe";
+                gold = cha;
+                break;
+        }
+    }
+
+    //To String Character Sheet
     public String toString() {
         return
         "Name: " + characterName + "\n" +
         "Sex: " + characterSex + "\n" +
         "Class: " + characterClass + "\n" +
-        "Attributes: \n" +
-        "Strength: " + str + " - Dexterity: " + dex + " - Wisdom: " + wis + " - Charisma: " + cha + "\n" + 
+        "Attributes: Strength " + str + " | Dexterity " + dex + " | Wisdom " + wis + " | Charisma " + cha + "\n" + 
         "Race: " + characterRace + "\n" +
-        "Movement: " + movement  
+        "Movement: " + String.format("%.1f", movement) + "\n" +
+        "Perícias: " + characterSkills + "\n" +
+        "Inventário: " + inventory + "\n" +
+        "Gold: " + String.format("%.2f", gold)
         ;
     }
 }
