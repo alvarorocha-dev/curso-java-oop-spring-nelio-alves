70b34906b0d84ddc0e89140a10749f2039e26c15
Alvaro Rocha <alvarowebrj@gmail.com>
2026-07-13 21:27:50 -0300

    Composição Exercicio Resolvido 02

---
+++ modulo12/ExemploComposicao02/entities/Comment.java
@@ -0,0 +1,21 @@
+package modulo12.ExemploComposicao02.entities;
+
+public class Comment {
+    
+    private String text;
+
+    public Comment() {
+    }
+
+    public Comment (String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }    
+}
